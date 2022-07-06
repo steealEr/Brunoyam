254435dfe778317056ca734b0f3bf70dfcfe3a1c
-steealEr <90792191+steealEr@users.noreply.github.com>
2022-07-06 21:25:36 +0300

    Add files via upload

---
+++ 05.07.2022/Rectangle.java
@@ -0,0 +1,25 @@
+public class Rectangle extends Square{
+    Rectangle(int length, int width) {
+        super(length, width);
+    }
+
+
+    public void perimeter() {
+        super.perimeter();
+    }
+
+
+    public void square() {
+        super.square();
+    }
+
+
+    public void setLength(int length) {
+        super.setLength(length);
+    }
+
+
+    public void setWidth(int width) {
+        super.setWidth(width);
+    }
+}
